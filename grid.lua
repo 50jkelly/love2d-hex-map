5bf9881be89c172b7f91bdc40409ae590a26bafb
Jonathan Kelly <50jkelly@gmail.com>
2017-12-19 13:25:14 +0000

    Added offset coordinate support and pixel to hex conversions

--- grid.lua
+++ grid.lua
@@ -1,5 +1,81 @@
 local this = {}
+local PI = 3.14159265359
+local size
 
+-- Helper functions
+
+local function round(n)
+  return math.floor(n + 0.5)
+end
+ 
+local function cube_round(cube)
+  local rx = round(cube.x)
+  local ry = round(cube.y)
+  local rz = round(cube.z)
+  
+  local x_diff = math.abs(rx - cube.x)
+  local y_diff = math.abs(ry - cube.y)
+  local z_diff = math.abs(rz - cube.z)
+  
+  if x_diff > y_diff and x_diff > z_diff then
+    rx = -ry - rz
+  elseif y_diff > z_diff then
+    ry = -rx - rz
+  else
+    rz = -rx - ry
+  end
+  
+  local out = {}
+  out.x = rx
+  out.y = ry
+  out.z = rz
+  
+  return out
+end
+
+local function hex_round(hex)
+  local cube = this.axial_to_cube(hex)
+  local result = cube_round(cube)
+  return this.cube_to_axial(result)
+end
+
+-- Hex dimensions
+
+function this.set_size(_size)
+  size = _size
+end
+
+function this.get_size()
+  return size
+end
+
+function this.get_width()
+  return round(math.sqrt(3)/2 * this.get_height())
+end
+
+function this.get_height()
+  return size * 2
+end
+
+-- Hex corner
+
+function this.get_corner(center, i)
+  local angle_degrees = 60 * i
+  local angle_radians = PI / 180 * angle_degrees
+  local point = {}
+  point.x = center.x + size * math.cos(angle_radians)
+  point.y = center.y + size * math.sin(angle_radians)
+  return point
+end
+
+-- Get pixel position of a hex
+
+function this.axial_to_pixel(hex)
+  local point = {}
+  point.x = size * math.sqrt(3) * (hex.q + hex.r / 2)
+  point.y = size * 3/2 * hex.r
+  return point
+end
 -- Conversion between coordinate systems
 
 function this.cube_to_axial(cube)
@@ -17,9 +93,24 @@ function this.axial_to_cube(hex)
   return cube
 end
 
+function this.cube_to_offset(cube)
+  local offset = {}
+  offset.col = cube.x + (cube.z - math.abs(cube.z) % 2) / 2
+  offset.row = cube.z
+  return offset
+end
+
+function this.offset_to_cube(offset)
+  local cube = {}
+  cube.x = offset.col - (offset.row - math.abs(offset.row) % 2) / 2
+  cube.z = offset.row
+  cube.y = -cube.x -cube.z
+  return cube
+end
+
 -- Get coordinates of neighbouring hex
 
-function this.cube_neighbour(cube, direction)
+function this.get_cube_neighbour(cube, direction)
   local directions = {}
   directions.north = {}
   directions.north.x = 0
@@ -58,7 +149,7 @@ function this.cube_neighbour(cube, direction)
   return result
 end
 
-function this.axial_neighbour(hex, direction)
+function this.get_axial_neighbour(hex, direction)
   local directions = {}
   directions.north = {}
   directions.north.q = 0
@@ -92,14 +183,14 @@ end
 
 -- Get distance between hexes
 
-function this.cube_distance(a, b)
+function this.get_cube_distance(a, b)
   return (
     math.abs(a.x - b.x) +
     math.abs(a.y - b.y) +
     math.abs(a.z - b.z)) / 2
 end
 
-function this.axial_distance(a, b)
+function this.get_axial_distance(a, b)
   local cube_a = this.axial_to_cube(a)
   local cube_b = this.axial_to_cube(b)
   return this.cube_distance(cube_a, cube_b)
@@ -107,7 +198,7 @@ end
   
 -- Return all hexes in a line between two hexes
 
-function this.cube_line(a, b)
+function this.get_cube_line(a, b)
   local function linear_interpolation(a, b, t)
     return a + (b - a) * t
   end
@@ -120,35 +211,6 @@ function this.cube_line(a, b)
     return cube
   end
   
-  local function round(n)
-    return math.floor(n + 0.5)
-  end
-  
-  local function cube_round(cube)
-    local rx = round(cube.x)
-    local ry = round(cube.y)
-    local rz = round(cube.z)
-    
-    local x_diff = math.abs(rx - cube.x)
-    local y_diff = math.abs(ry - cube.y)
-    local z_diff = math.abs(rz - cube.z)
-    
-    if (x_diff > y_diff and x_diff > z_diff) then
-      rx = -ry - rz
-    elseif (y_diff > z_diff) then
-      ry = -rx - rz
-    else
-      rz = -rx - ry
-    end
-    
-    local out = {}
-    out.x = rx
-    out.y = ry
-    out.z = rz
-    
-    return out
-  end
-  
   local N = this.cube_distance(a, b)
   local results = {}
   for i=0,N do
@@ -158,7 +220,7 @@ function this.cube_line(a, b)
   return results
 end
 
-function this.axial_line(a, b)
+function this.get_axial_line(a, b)
   local cube_a = this.axial_to_cube(a)
   local cube_b = this.axial_to_cube(b)
   local cube_results = this.cube_line(cube_a, cube_b)
@@ -169,6 +231,21 @@ function this.axial_line(a, b)
   end
   
   return results
+end
+
+
+-- Return the grid coordinates of the given pixel position
+
+function this.pixel_to_axial(x, y)
+  local hex = {}
+  hex.q = (1/3 * math.sqrt(3) * x - 1/3 * y) / size
+  hex.r = 2/3 * y / size
+  return hex_round(hex)
+end
+
+function this.pixel_to_cube(x, y)
+  local hex = this.pixel._to_axial(x, y)
+  return this.axial_to_cube(hex)
 end
 
 return this
--- main.lua
+++ main.lua
@@ -1,26 +1,46 @@
-local hex = require 'hex'
 local grid = require 'grid'
 
+local hex_image
+
+function love.load()
+  hex_image = love.graphics.newImage('HK-Fantasyland/HK - Blank/HK_blank_003.png')
+  love.window.setMode(800, 600, { resizable=true })
+  love.window.maximize()
+end
+
 function love.draw()
-  local text_y = 200
+  local h = {}
   
-  local a = {}
-  a.x = 0
-  a.y = 0
-  a.z = 0
-  
-  local b = {}
-  b.x = -2
-  b.y = 3
-  b.z = -1
-  
-  for i,cube in ipairs(grid.cube_line(a, b)) do
-    love.graphics.print(
-      cube.x .. ',' .. cube.y .. ',' .. cube.z,
-      400,
-      text_y)
-    
-    text_y = text_y + 20
+  for i = 1, 5 do
+    for j = 1, 5 do
+      local offset = {}
+      offset.row = i
+      offset.col = j
+      
+      local new_cube = grid.offset_to_cube(offset)
+      table.insert(h, grid.cube_to_axial(new_cube))
+    end
   end
+    
+  grid.set_size(95)
+  
+  local mouse_x, mouse_y = love.mouse.getPosition()
+  local mouse_hex = grid.pixel_to_axial(mouse_x, mouse_y)
+  
+  for i, e in ipairs(h) do
+    local position = grid.axial_to_pixel(e)
+    
+    if (e.q == mouse_hex.q and e.r == mouse_hex.r) then
+      love.graphics.setColor(0,0,255)
+    else
+      love.graphics.setColor(255,255,255)
+    end
+  
+    love.graphics.draw(hex_image, position.x - grid.get_size(), position.y - grid.get_size())
+  end
+  
+  love.graphics.setColor(255,0,0)
+  love.graphics.circle('fill', mouse_x, mouse_y, 3)
+  love.graphics.print(mouse_hex.q .. ',' .. mouse_hex.r, 400, 20)
 end
 
